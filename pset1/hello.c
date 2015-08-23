d0a5949f858ceabfc1cc585a2764cc7b7e3c13c2
Simon Lee <silee001@gmail.com>
2015-08-23 15:20:27 -0400

    Added hello.c to pset1

---
+++ pset1/hello.c
@@ -0,0 +1,18 @@
+/**
+ * hello.c
+ *
+ * Simon Lee
+ * silee001@gmail.com
+ *
+ * Prints hello world to stdio
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+
+    return 0;
+}
